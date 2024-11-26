8be29295a8d5bd3d4b7005ab89aca40edd1194ff
kawthar-sultan <codebraver57@codebrave.org>
2024-11-26 11:16:18 +0200

    Add files via upload

---
+++ lebanon/script.js
@@ -0,0 +1,45 @@
+/* baalbek */
+function img_flipp() { 
+    document.getElementById('baalbek-back').className = 'flipp';
+} 
+    
+function img_flippBack() {
+     document.getElementById('baalbek-back').className = '';
+}
+
+/* sidon */
+function img_flipp1() {
+    document.getElementById('sidon').className = 'flipp';
+}
+function img_flippBack1() {
+    document.getElementById('sidon').className = '';
+}
+/* frata */
+function img_flipp2() {
+    document.getElementById('faraya-back').className = 'flipp';
+}
+function img_flippBack2() {
+    document.getElementById('faraya-back').className = '';
+}
+/* jeita */
+function img_flipp3() {
+    document.getElementById('jeita').className = 'flipp';
+}
+function img_flippBack3() {
+    document.getElementById('jeita').className = '';
+}
+/* zaituna bay */
+function img_flipp4() {
+    document.getElementById('zaituna-bay-back').className = 'flipp';
+}
+function img_flippBack4() {
+    document.getElementById('zaituna-bay-back').className = '';
+}
+
+/* byblos */
+function img_flipp5() {
+    document.getElementById('byblos').className = 'flipp';
+}
+function img_flippBack5() {
+    document.getElementById('byblos').className = '';
+}
